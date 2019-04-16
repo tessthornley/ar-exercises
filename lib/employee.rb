d3a0831c70c9c200d181d6ea5bb767072f390082
Tess <tess.thornley@gmail.com>
2019-04-16 17:21:09 +0000

    exercise 7 completed:

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,12 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_numericality_of :hourly_rate, :only_integer => true, :in => 40..200
+  validates :store, presence: true
 end
+
+#  * Employees must always have a first name present
+#  * Employees must always have a last name present
+#  * Employees have a hourly_rate that is a number (integer) between 40 and 200
+#  * Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,9 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates_numericality_of :annual_revenue, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_associated :employees
 end
+
+#  * Stores must always have a name that is a minimum of 3 characters
+#  * Stores have an annual_revenue that is a number (integer) that must be 0 or more
